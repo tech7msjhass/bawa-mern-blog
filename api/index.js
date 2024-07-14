9f738fdfe096d62e907bfa345c0aa1edb72e4308
tech7msjhass <jhassmandeepsingh@gmail.com>
2024-07-14 22:03:57 +0530

    created middlewares for handling error

--- api/index.js
+++ api/index.js
@@ -25,3 +25,14 @@ app.listen(4000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
